c20241b93052fe1020495685c16198825b6f6819
442003360 <91403639+442003360@users.noreply.github.com>
2021-12-13 20:53:44 +0300

    Create age .java

---
+++ age .java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	   System.out.println("entar age");
+	   Scanner srt = new Scanner(System.in);
+	   int num = srt.nextInt();
+	    num = 2021-num;
+	   System.out.println(num);
+	   
+		
+	}
+}
