49f212b1d4733475e678595dd4ec25e50743ea2b
442003360 <91403639+442003360@users.noreply.github.com>
2021-12-13 21:28:11 +0300

    Create radius.java

---
+++ radius.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner srt = new Scanner(System.in);
+	    float radius=3.14F;
+	    System.out.println("enter ocean");
+	    float ocean=srt.nextFloat();
+	    System.out.println(radius *ocean*ocean);
+	    
+		
+	}
+}
