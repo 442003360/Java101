c94f3b68a272a2ab05a339c78872f63f69fb9614
442003360 <91403639+442003360@users.noreply.github.com>
2021-12-13 23:35:14 +0300

    Create positiv and Negativ.java

---
+++ positiv and Negativ.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter number");
+	int x=scan.nextInt();
+	
+	
+	
+	if(x>=0){System.out.println("positiv number");}
+	else System.out.println("negativ number");
+	}
