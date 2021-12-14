0a6da7d72756b97db8cadb29f99d3c181486384e
442003360 <91403639+442003360@users.noreply.github.com>
2021-12-14 03:45:59 +0300

    Create two digit subtraction funcction.java

---
+++ two digit subtraction funcction.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    static int maiuns(int x ,int y) {
+        return  x-y;
+    }
+	public static void main(String[] args) {
+	    System.out.println(maiuns(5,3));
+		
+	}
+}
