dd9d8acc5987fe26e1acd6993aeea4d325695d11
442003360 <91403639+442003360@users.noreply.github.com>
2021-12-15 04:17:32 +0300

    Update and rename two digit subtraction funcction.java to subtrac tow number Funcion.java

---
+++ subtrac tow number Funcion.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    static int minus(int x ,int y) {
+        return  x-y;
+    }
+    static int plus(int r,int f){
+        return r+f;
+        
+    }
+	public static void main(String[] args) {
+	    System.out.println(minus(5,3));
+	    System.out.println(plus(2,2));
+
+	}
+}
