0180eb07afe1363c03e49f1c7d1b1ef6ff1a203c
442003360 <91403639+442003360@users.noreply.github.com>
2021-12-15 07:02:47 +0300

    Create sqarw widthand heghith.java

---
+++ sqarw widthand heghith.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner srt = new Scanner(System.in);
+	    System.out.println("enterr height");
+	    int height=srt.nextInt();
+	    System.out.println("enterr the offer");
+	    int theoffer=srt.nextInt();
+	    System.out.println(height*theoffer);
+	    
+		
+	}
+}
