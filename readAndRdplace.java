b493a0b7d33c33ccec77cb051b4d572850d04205
442003360 <91403639+442003360@users.noreply.github.com>
2021-12-15 16:46:20 +0300

    Create readAndRdplace.java

---
+++ readAndRdplace.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String srt =" mohammad";
+	    for(int x = 0;x<=8;x++){
+	    if(srt.charAt(x)=='m'){
+	        System.out.print('s');};
+	        System.out.print(srt.charAt(x));
+	        
+	   
+	     
+	       
+	   } 
+	}
+}
